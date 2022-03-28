8d32169813c8b50a6e992743862d4d37d8db9a51
noureddineait <noureddinelhaj@gmail.com>
2022-03-28 13:05:36 -0400

    Add project files.

---
+++ Conversion/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Conversion.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult FirstPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FirstPage(Models.Conversion conversion)
+        {
+            if (ModelState.IsValid)
+                return RedirectToAction("ConversionPage", conversion);
+            return View();
+        }
+        public IActionResult ConversionPage(Models.Conversion conversion)
+        {
+
+            if (conversion.InitialMetric == "Celsius")
+            {
+                conversion.FinalMetric = "Fahrenheit";
+                conversion.FinalValue = (conversion.InitialValue * 9/ 5) +32;
+            }
+            if (conversion.InitialMetric == "Fahrenheit")
+            {
+                conversion.FinalMetric = "Celsius";
+                conversion.FinalValue = (conversion.InitialValue -32)*5/9;
+            }
+            return View(conversion);
+        }
+    }
+}
---
+++ Conversion/Models/Conversion.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conversion.Models
+{
+    public class Conversion
+    {
+        [Required(ErrorMessage = "Champs requis !")]
+        public string? InitialMetric { get; set; }
+        [Required(ErrorMessage = "Champs requis !")]
+        public int? InitialValue { get; set; }
+
+        public string? FinalMetric { get; set; }
+
+        public int? FinalValue { get; set; }
+    }
+
+}
